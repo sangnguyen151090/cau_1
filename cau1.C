191f4f68c327d933008183262b87c915c567a061
sangnguyen151090 <83156738+sangnguyen151090@users.noreply.github.com>
2021-07-04 13:38:34 +0700

    Add files via upload

---
+++ cau1.C
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+int value = 0;
+typedef union 
+{
+	uint16_t value;
+	uint8_t byte[2];
+}tach_byte_t;
+
+void main()
+{
+	printf("nhap vao gia trị của value:");
+	scanf_s("%d", &value);
+	tach_byte_t A;
+	A.value = value;
+	printf("low_bit: 0x%X,high_bit:0x%X\r\n", A.byte[0], A.byte[1]);
+
+}
